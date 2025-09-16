2411cde6906d27e771f9403d58ccb574cc9a61ba
Greencoders06 <vaibhavupadhyay2408@gmail.com>
2025-09-17 00:10:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠️ Do NOT share this file publicly. Anyone can see these keys if deployed.
+// ⚠️ Do NOT share this file publicly. Anyone can see this key if deployed.
+window.OPENAI_API_KEY = "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"
+
+
